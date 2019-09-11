60c97dd168eb20e53ceed48b06d73c006b6df55f
shjang7 <lucy.sh.jang@gmail.com>
2019-09-12 06:10:42 +0900

    Update facebook image for large size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,12 +2,12 @@
 
 module UsersHelper
   # Returns the profile image for the given user.
-  def profile_img_for(user, size = 80)
+  def profile_img_for(user)
     profile_url = if user.profile_pic.nil?
                     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-                    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+                    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
                   else
-                    user.profile_pic.sub('_normal', '')
+                    user.profile_pic + '?type=large'
                   end
     image_tag(profile_url, alt: user.name, class: 'profile-pic')
   end
